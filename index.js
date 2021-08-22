2bfeffc0326ebd8c3a4af2fa4a209452947a7640
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-08-22 22:08:15 +0600

    Dispatching Action

--- index.js
+++ index.js
@@ -6,9 +6,32 @@ const initState={
 }
 //Reducer
 const rootReducer=(state=initState,action)=>{
+  if(action.type==='INC_NUM'){
+   return{
+     ...state,
+     num:state.num+1
+   }
+
+  }
+  if(action.type==='ADD_NUM'){
+    return{
+     ...state,
+     num:state.num+action.value
+
+    }
+  }
   return state;
 }
 
 //Store
 const store=redux.createStore(rootReducer);
+//console.log(store.getState());
+//Dispatching
+store.dispatch({
+  type:'INC_NUM'
+});
+store.dispatch({
+  type:'ADD_NUM',
+  value:34
+})
 console.log(store.getState());
